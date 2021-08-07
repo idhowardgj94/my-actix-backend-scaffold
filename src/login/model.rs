3079acdc75408ed8ff9bb9012e57087fdec0f5f5
howard chang <idhowardgj94@gmail.com>
2021-08-07 23:46:20 +0800

    test to convert timestamp to tw time use chrono

--- src/login/model.rs
+++ src/login/model.rs
@@ -21,5 +21,20 @@ impl From<&User> for BTreeMap<String, String> {
 #[derive(Debug, Deserialize, Serialize)]
 pub struct UserProfile {
     pub name: String,
+    // pub expire_date: Date
 }
 
+#[cfg(test)]
+mod testChrono {
+    use chrono::{DateTime, Local, NaiveDateTime, Utc, FixedOffset, LocalResult};
+
+    #[test]
+    pub fn test_timestamp() {
+        // let timestamp = 1572580800;
+        let nano: i64 = 1628348358004 % 1000;
+        let hour = 3600;
+        let d = DateTime::<Local>::from_utc(NaiveDateTime::from_timestamp(1628348358004/1000, nano as u32), FixedOffset::east(8 * hour));
+        panic!("{:?}", d);
+        // TODO: this is how to use TimeStamp and convert to TW time
+    }
+}
